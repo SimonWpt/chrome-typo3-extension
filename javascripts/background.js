89b720c5332229d64a855b4cfebe0d6fe78e0002
Bertram Simon <info@agentur-simon.de>
2015-03-22 18:52:50 +0100

    Update: jQuery 1.11.2 Update: Bootstrao v3.3.4 Minor Updates and Improvments

--- javascripts/background.js
+++ javascripts/background.js
@@ -1,14 +1,14 @@
 /*
-#
-# TYPO3 - Little helpers
-#
-# Function: creating context-menus
-# Copyright 2012 by Agentur Simon / Bertram Simon / www.agentur-simon.de
-#
-*/
+ #
+ # TYPO3 - Little helpers
+ #
+ # Function: creating context-menus
+ # Copyright 2012 by Agentur Simon / Bertram Simon / www.agentur-simon.de
+ #
+ */
 
 
-(function() {
+(function () {
   var t3top;
 
   t3top = chrome.contextMenus.create({
@@ -20,7 +20,7 @@
     title: "Search Documentation",
     contexts: ["selection"],
     parentId: t3top,
-    onclick: function(info, tab) {
+    onclick: function (info, tab) {
       return chrome.tabs.create({
         url: 'http://typo3.org/search/?q=' + info.selectionText + '&tx_solr%5Bfilter%5D%5B1%5D=type%253Atx_terdoc_manuals',
         index: tab.index + 1
@@ -32,7 +32,7 @@
     title: "Search Extensions",
     contexts: ["selection"],
     parentId: t3top,
-    onclick: function(info, tab) {
+    onclick: function (info, tab) {
       return chrome.tabs.create({
         url: 'http://typo3.org/search/?q=' + info.selectionText + '&tx_solr%5Bfilter%5D%5B0%5D=type%253Atx_terfe2_domain_model_extension',
         index: tab.index + 1
@@ -44,7 +44,7 @@
     title: "Search Wiki",
     contexts: ["selection"],
     parentId: t3top,
-    onclick: function(info, tab) {
+    onclick: function (info, tab) {
       return chrome.tabs.create({
         url: 'http://wiki.typo3.org/wiki/index.php?search=' + info.selectionText + '&title=Special%3ASearch&fulltext=Search',
         index: tab.index + 1
@@ -55,7 +55,7 @@
   chrome.contextMenus.create({
     title: "Login",
     parentId: t3top,
-    onclick: function(info, tab) {
+    onclick: function (info, tab) {
       return chrome.tabs.create({
         url: tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[0] + '/typo3/index.php',
         index: tab.index + 1
@@ -66,7 +66,7 @@
   chrome.contextMenus.create({
     title: "Installtool",
     parentId: t3top,
-    onclick: function(info, tab) {
+    onclick: function (info, tab) {
       return chrome.tabs.create({
         url: tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[0] + '/typo3/sysext/install/Start/Install.php',
         index: tab.index + 1
--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,49 +1,56 @@
 /*
-# 
-# TYPO3 - Little helpers
-# 
-# Function: reading + display version
-# Copyright 2012 by Agentur Simon / Bertram Simon / www.agentur-simon.de
-#
-*/
+ #
+ # TYPO3 CMS - Little helpers
+ #
+ # Function: reading + display version
+ # Copyright 2015 by Agentur Simon / Bertram Simon / www.agentur-simon.de
+ #
+ */
 
 
-(function() {
-  $(document).ready(function() {
-    var openTab;
+(function () {
+  $(document).ready(
+    function openTab() {
+      var openTab;
 
-    openTab = function(localUrl) {
-      window.localUrl = localUrl;
-      return chrome.tabs.getSelected(null, function(tab) {
-        return chrome.tabs.create({
-          url: tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[0] + "/typo3" + window.localUrl,
-          index: tab.index + 1
+      openTab = function (localUrl) {
+        window.localUrl = localUrl;
+        return chrome.tabs.getSelected(null, function (tab) {
+          return chrome.tabs.create({
+            url: tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[0] + "/typo3" + window.localUrl,
+            index: tab.index + 1
+          });
         });
+      };
+      $.getJSON("manifest.json", function (data) {
+        return $("span.version").text("[version: " + data.version + "]");
       });
-    };
-    $.getJSON("manifest.json", function(data) {
-      return $("span.version").text("[version: " + data.version + "]");
+      $('#t3news').FeedEk({
+        FeedUrl: "http://typo3.org/xml-feeds/rss.xml",
+        MaxCount: 10,
+        TitleLinkTarget: '_blank',
+        Header: "Latest News"
+      });
+      $('#t3ver').FeedEk({
+        FeedUrl: "http://sourceforge.net/projects/typo3/rss",
+        MaxCount: 10,
+        TitleLinkTarget: '_blank',
+        Header: "Latest versions"
+      });
+      $(".typo3-login").click(function () {
+        return openTab("/index.php");
+      });
+      $(".typo3-installtool").click(function () {
+        return openTab("/install/index.php");
+      });
+      console.log("\n" +
+      "  _/_/_/_/_/  _/      _/  _/_/_/      _/_/    _/_/_/\n" +
+      "     _/        _/  _/    _/    _/  _/    _/        _/\n" +
+      "    _/          _/      _/_/_/    _/    _/    _/_/\n" +
+      "   _/          _/      _/        _/    _/        _/\n" +
+      "  _/          _/      _/          _/_/    _/_/_/\n\n" +
+      "  © 2015 TYPO3 CMS - Little Helper by Bertram Simon / Agentur Simon\n\n\n\n");
+      return $('[data-toggle="tooltip"]').tooltip();
     });
-    $('#t3news').FeedEk({
-      FeedUrl: "http://typo3.org/xml-feeds/rss.xml",
-      MaxCount: 10,
-      TitleLinkTarget: '_blank',
-      Header: "Latest News"
-    });
-    $('#t3ver').FeedEk({
-      FeedUrl: "http://sourceforge.net/projects/typo3/rss",
-      MaxCount: 10,
-      TitleLinkTarget: '_blank',
-      Header: "Latest versions"
-    });
-    $(".typo3-login").click(function() {
-      return openTab("/index.php");
-    });
-    $(".typo3-installtool").click(function() {
-      return openTab("/install/index.php");
-    });
-    console.log("\n  _/_/_/_/_/  _/      _/  _/_/_/      _/_/    _/_/_/\n     _/        _/  _/    _/    _/  _/    _/        _/\n    _/          _/      _/_/_/    _/    _/    _/_/\n   _/          _/      _/        _/    _/        _/\n  _/          _/      _/          _/_/    _/_/_/\n  \n  © 2014 TYPO3 Little Helper by Bertram Simon / Agentur Simon\n  ");
-    return $('[data-toggle="tooltip"]').tooltip();
-  });
 
 }).call(this);
