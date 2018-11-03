87bf3d9fb9285a5c0097136e18caf14a54df0b35
Bertram Simon <b.simon@agentur-simon.de>
2018-11-03 16:31:09 +0100

    [TASK] Cleanup

--- javascripts/jquery.jfeed.js
+++ javascripts/jquery.jfeed.js
@@ -1,21 +1,12 @@
-/*
- *
- * @author Bertram Simon <b.simon@agentur-simon.de>
- * @copyright 2018 by Bertram Simon
- * @filename jquery.jfeed.js
- * @lastmodfied 25.04.18 20:19
- *
- */
-
 /* jFeed : jQuery feed parser plugin
  * Copyright (C) 2007 Jean-François Hovinne - http://www.hovinne.com/
  * Dual licensed under the MIT (MIT-license.txt)
  * and GPL (GPL-license.txt) licenses.
  */
 
-jQuery.getFeed = function (options) {
+$.getFeed = function (options) {
 
-  options = jQuery.extend({
+  options = $.extend({
 
     url: null,
     data: null,
@@ -29,12 +20,12 @@ jQuery.getFeed = function (options) {
 
   if (options.url) {
 
-    if (jQuery.isFunction(options.failure) && jQuery.type(options.error) === 'null') {
+    if (typeof(options.failure) === "function" && $.type(options.error) === 'null') {
       // Handle legacy failure option
       options.error = function (xhr, msg, e) {
         options.failure(msg, e);
       }
-    } else if (jQuery.type(options.failure) === jQuery.type(options.error) === 'null') {
+    } else if ($.type(options.failure) === $.type(options.error) === 'null') {
       // Default error behavior if failure & error both unspecified
       options.error = function (xhr, msg, e) {
         window.console && console.log('getFeed failed to load feed', xhr, msg, e);
@@ -48,8 +39,8 @@ jQuery.getFeed = function (options) {
       cache: options.cache,
       dataType: "xml",
       success: function (xml) {
-        var feed = new JFeed(xml);
-        if (jQuery.isFunction(options.success)) options.success(feed);
+        const feed = new JFeed(xml);
+        if (typeof(options.success) === "function") options.success(feed);
       },
       error: options.error,
       global: options.global
@@ -60,6 +51,7 @@ jQuery.getFeed = function (options) {
 function JFeed(xml) {
   if (xml) this.parse(xml);
 }
+
 JFeed.prototype = {
 
   type: '',
@@ -70,23 +62,24 @@ JFeed.prototype = {
   parse: function (xml) {
 
 
-    if (jQuery('channel', xml).length == 1) {
+    if ($('channel', xml).length === 1) {
 
       this.type = 'rss';
       var feedClass = new JRss(xml);
 
-    } else if (jQuery('feed', xml).length == 1) {
+    } else if ($('feed', xml).length === 1) {
 
       this.type = 'atom';
       var feedClass = new JAtom(xml);
     }
 
-    if (feedClass) jQuery.extend(this, feedClass);
+    if (feedClass) $.extend(this, feedClass);
   }
 };
 
 function JFeedItem() {
 }
+
 JFeedItem.prototype = {
 
   title: '',
@@ -99,32 +92,33 @@ JFeedItem.prototype = {
 function JAtom(xml) {
   this._parse(xml);
 }
+
 JAtom.prototype = {
 
   _parse: function (xml) {
 
-    var channel = jQuery('feed', xml).eq(0);
+    var channel = $('feed', xml).eq(0);
 
     this.version = '1.0';
-    this.title = jQuery(channel).find('title:first').text();
-    this.link = jQuery(channel).find('link:first').attr('href');
-    this.description = jQuery(channel).find('subtitle:first').text();
-    this.language = jQuery(channel).attr('xml:lang');
-    this.updated = jQuery(channel).find('updated:first').text();
+    this.title = $(channel).find('title:first').text();
+    this.link = $(channel).find('link:first').attr('href');
+    this.description = $(channel).find('subtitle:first').text();
+    this.language = $(channel).attr('xml:lang');
+    this.updated = $(channel).find('updated:first').text();
 
     this.items = [];
 
     var feed = this;
 
-    jQuery('entry', xml).each(function () {
+    $('entry', xml).each(function () {
 
       var item = new JFeedItem();
 
-      item.title = jQuery(this).find('title').eq(0).text();
-      item.link = jQuery(this).find('link').eq(0).attr('href');
-      item.description = jQuery(this).find('content').eq(0).text();
-      item.updated = jQuery(this).find('updated').eq(0).text();
-      item.id = jQuery(this).find('id').eq(0).text();
+      item.title = $(this).find('title').eq(0).text();
+      item.link = $(this).find('link').eq(0).attr('href');
+      item.description = $(this).find('content').eq(0).text();
+      item.updated = $(this).find('updated').eq(0).text();
+      item.id = $(this).find('id').eq(0).text();
 
       feed.items.push(item);
     });
@@ -134,34 +128,35 @@ JAtom.prototype = {
 function JRss(xml) {
   this._parse(xml);
 }
+
 JRss.prototype = {
 
   _parse: function (xml) {
 
-    if (jQuery('rss', xml).length == 0) this.version = '1.0';
-    else this.version = jQuery('rss', xml).eq(0).attr('version');
+    if ($('rss', xml).length === 0) this.version = '1.0';
+    else this.version = $('rss', xml).eq(0).attr('version');
 
-    var channel = jQuery('channel', xml).eq(0);
+    var channel = $('channel', xml).eq(0);
 
-    this.title = jQuery(channel).find('title:first').text();
-    this.link = jQuery(channel).find('link:first').text();
-    this.description = jQuery(channel).find('description:first').text();
-    this.language = jQuery(channel).find('language:first').text();
-    this.updated = jQuery(channel).find('lastBuildDate:first').text();
+    this.title = $(channel).find('title:first').text();
+    this.link = $(channel).find('link:first').text();
+    this.description = $(channel).find('description:first').text();
+    this.language = $(channel).find('language:first').text();
+    this.updated = $(channel).find('lastBuildDate:first').text();
 
     this.items = [];
 
     var feed = this;
 
-    jQuery('item', xml).each(function () {
+    $('item', xml).each(function () {
 
       var item = new JFeedItem();
 
-      item.title = jQuery(this).find('title').eq(0).text();
-      item.link = jQuery(this).find('link').eq(0).text();
-      item.description = jQuery(this).find('description').eq(0).text();
-      item.updated = jQuery(this).find('pubDate').eq(0).text();
-      item.id = jQuery(this).find('guid').eq(0).text();
+      item.title = $(this).find('title').eq(0).text();
+      item.link = $(this).find('link').eq(0).text();
+      item.description = $(this).find('description').eq(0).text();
+      item.updated = $(this).find('pubDate').eq(0).text();
+      item.id = $(this).find('guid').eq(0).text();
 
       feed.items.push(item);
     });
